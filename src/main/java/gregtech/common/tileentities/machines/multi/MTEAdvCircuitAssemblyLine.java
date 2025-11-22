975aaeff158c2b29c253c5f8c44d8ad140e19d9f
mamiemru <mrunder.s@gmail.com>
2025-11-22 17:20:12 +0100

    chore: update structure and recipe

--- src/main/java/gregtech/common/tileentities/machines/multi/MTEAdvCircuitAssemblyLine.java
+++ src/main/java/gregtech/common/tileentities/machines/multi/MTEAdvCircuitAssemblyLine.java
@@ -2,8 +2,6 @@ package gregtech.common.tileentities.machines.multi;
 
 import static com.gtnewhorizon.structurelib.structure.StructureUtility.ofBlock;
 import static com.gtnewhorizon.structurelib.structure.StructureUtility.ofChain;
-import static com.gtnewhorizon.structurelib.structure.StructureUtility.transpose;
-import static gregtech.api.enums.HatchElement.Energy;
 import static gregtech.api.enums.HatchElement.InputBus;
 import static gregtech.api.enums.HatchElement.InputHatch;
 import static gregtech.api.enums.HatchElement.Maintenance;
@@ -11,15 +9,24 @@ import static gregtech.api.enums.HatchElement.OutputBus;
 import static gregtech.api.enums.Textures.BlockIcons.OVERLAY_FRONT_ASSEMBLY_LINE;
 import static gregtech.api.enums.Textures.BlockIcons.OVERLAY_FRONT_ASSEMBLY_LINE_ACTIVE;
 import static gregtech.api.enums.Textures.BlockIcons.OVERLAY_FRONT_ASSEMBLY_LINE_ACTIVE_GLOW;
-import static gregtech.api.enums.Textures.BlockIcons.OVERLAY_FRONT_ASSEMBLY_LINE_GLOW;
+import static gregtech.api.enums.Textures.BlockIcons.casingTexturePages;
 import static gregtech.api.recipe.RecipeMaps.advCircuitAssemblylineRecipes;
 import static gregtech.api.util.GTStructureUtility.buildHatchAdder;
 import static gregtech.api.util.GTStructureUtility.chainAllGlasses;
+import static gregtech.common.misc.WirelessNetworkManager.addEUToGlobalEnergyMap;
+import static gregtech.common.misc.WirelessNetworkManager.getUserEU;
+import static gregtech.common.misc.WirelessNetworkManager.processInitialSettings;
 
-import java.util.Set;
+import java.math.BigInteger;
+import java.util.UUID;
 
+import javax.annotation.Nonnull;
+
+import gregtech.api.util.GTUtility;
 import net.minecraft.item.ItemStack;
 import net.minecraftforge.common.util.ForgeDirection;
+
+import org.jetbrains.annotations.NotNull;
 
 import com.gtnewhorizon.structurelib.alignment.constructable.ISurvivalConstructable;
 import com.gtnewhorizon.structurelib.structure.IStructureDefinition;
@@ -27,38 +34,73 @@ import com.gtnewhorizon.structurelib.structure.ISurvivalBuildEnvironment;
 import com.gtnewhorizon.structurelib.structure.StructureDefinition;
 
 import gregtech.api.GregTechAPI;
-import gregtech.api.enums.Textures.BlockIcons;
-import gregtech.api.enums.VoidingMode;
 import gregtech.api.interfaces.ITexture;
 import gregtech.api.interfaces.metatileentity.IMetaTileEntity;
 import gregtech.api.interfaces.tileentity.IGregTechTileEntity;
 import gregtech.api.logic.ProcessingLogic;
-import gregtech.api.metatileentity.implementations.MTEExtendedPowerMultiBlockBase;
+import gregtech.api.metatileentity.implementations.MTEEnhancedMultiBlockBase;
 import gregtech.api.recipe.RecipeMap;
+import gregtech.api.recipe.check.CheckRecipeResult;
+import gregtech.api.recipe.check.CheckRecipeResultRegistry;
 import gregtech.api.render.TextureFactory;
+import gregtech.api.util.GTRecipe;
+import gregtech.api.util.GTRecipeConstants;
 import gregtech.api.util.MultiblockTooltipBuilder;
+import gregtech.api.util.OverclockCalculator;
+import gregtech.common.gui.modularui.multiblock.MTEAdvancedCircuitAssemblyLineGui;
+import gregtech.common.gui.modularui.multiblock.base.MTEMultiBlockBaseGui;
 import gregtech.common.misc.GTStructureChannels;
 
-public class MTEAdvCircuitAssemblyLine extends MTEExtendedPowerMultiBlockBase<MTEAdvCircuitAssemblyLine>
+public class MTEAdvCircuitAssemblyLine extends MTEEnhancedMultiBlockBase<MTEAdvCircuitAssemblyLine>
     implements ISurvivalConstructable {
 
+    private UUID ownerUUID;
+    private int multiplier = 1;
+    BigInteger finalConsumption = BigInteger.ZERO;
     private static final String STRUCTURE_PIECE_MAIN = "main";
+
+    private static final int HORIZONTAL_OFFSET = 3;
+    private static final int VERTICAL_OFFSET = 3;
+    private static final int DEPTH_OFFSET = 1;
+    private static final String[][] structure = new String[][] {
+        { "BBBBBBB", "B     B", "B     B", "B     B", "B     B", "B     B", "BBBBBBB" },
+        { "BBBBBBB", "BGGGGGB", "BGGGGGB", "BGG~GGB", "BGGGGGB", "BGGGGGB", "BBBBBBB" },
+        { "BAAAAAB", "A CEC A", "A     A", "AE F EA", "A     A", "A CEC A", "BBBBBBB" },
+        { "BAAAAAB", "A CEC A", "A CCC A", "AECFCEA", "A CCC A", "A CEC A", "BBBBBBB" },
+        { "BAAAAAB", "A CEC A", "A     A", "AE F EA", "A     A", "A CEC A", "BBBBBBB" },
+        { "BAAAAAB", "A CEC A", "A     A", "AE F EA", "A     A", "A CEC A", "BBBBBBB" },
+        { "BBBBBBB", "BECECEB", "BEFFFEB", "BEFFFEB", "BEFFFEB", "BECECEB", "BBBBBBB" },
+        { "BAAAAAB", "A CEC A", "A     A", "AE F EA", "A     A", "A CEC A", "BBBBBBB" },
+        { "BAAAAAB", "A CEC A", "A     A", "AE F EA", "A     A", "A CEC A", "BBBBBBB" },
+        { "BAAAAAB", "A CEC A", "A CCC A", "AECFCEA", "A CCC A", "A CEC A", "BBBBBBB" },
+        { "BAAAAAB", "A CEC A", "A     A", "AE F EA", "A     A", "A CEC A", "BBBBBBB" },
+        { "BBBBBBB", "BBBBBBB", "BBDDDBB", "BBDODBB", "BBDDDBB", "BBCECBB", "BBBBBBB" },
+        { "       ", " BBBBB ", " B   B ", " B   B ", " B   B ", " BBBBB ", " BBBBB " } };
+
     private static final IStructureDefinition<MTEAdvCircuitAssemblyLine> STRUCTURE_DEFINITION = StructureDefinition
         .<MTEAdvCircuitAssemblyLine>builder()
-        .addShape(
-            STRUCTURE_PIECE_MAIN,
-            transpose(new String[][] { { "ccc", "cCc", "ccc" }, { "CCC", "C-C", "CCC" }, { "b~b", "bbb", "bbb" } }))
-        .addElement('c', ofBlock(GregTechAPI.sBlockCasings2, 9)) // assembler machine casing
-        .addElement('C', chainAllGlasses())
+        .addShape(STRUCTURE_PIECE_MAIN, structure)
+        .addElement('A', chainAllGlasses())
+        .addElement('B', ofBlock(GregTechAPI.sBlockCasings2, 0))
+        .addElement('C', ofBlock(GregTechAPI.sBlockCasings2, 5))
+        .addElement('D', ofBlock(GregTechAPI.sBlockCasings8, 12))
+        .addElement('E', ofBlock(GregTechAPI.sBlockCasings8, 14))
+        .addElement('F', ofBlock(GregTechAPI.sBlockCasings2, 9))
         .addElement(
-            'b',
+            'O',
             ofChain(
-                buildHatchAdder(MTEAdvCircuitAssemblyLine.class)
-                    .atLeast(Maintenance, InputBus, InputHatch, OutputBus, Energy)
-                    .casingIndex(11)
+                buildHatchAdder(MTEAdvCircuitAssemblyLine.class).atLeast(OutputBus)
+                    .casingIndex(GTUtility.getCasingTextureIndex(GregTechAPI.sBlockCasings8, 12))
+                    .dot(2)
+                    .build()))
+        .addElement(
+            'G',
+            ofChain(
+                buildHatchAdder(MTEAdvCircuitAssemblyLine.class).atLeast(Maintenance, InputBus, InputHatch)
+                    .casingIndex(GTUtility.getCasingTextureIndex(GregTechAPI.sBlockCasings8, 12))
                     .dot(1)
                     .build(),
-                ofBlock(GregTechAPI.sBlockCasings1, 11)))
+                ofBlock(GregTechAPI.sBlockCasings8, 12)))
         .build();
 
     public MTEAdvCircuitAssemblyLine(int aID, String aName, String aNameRegional) {
@@ -78,25 +120,17 @@ public class MTEAdvCircuitAssemblyLine extends MTEExtendedPowerMultiBlockBase<MT
     protected MultiblockTooltipBuilder createTooltip() {
         final MultiblockTooltipBuilder tt = new MultiblockTooltipBuilder();
         tt.addMachineType("Advanced Circuit Assembly Line, Never Actually Coming, AdvCAL")
-            .addMaxTierSkips(1)
-            .beginStructureBlock(3, 5, 3, true)
-            .addController("Front bottom")
-            .addCasingInfoRange("Heat Proof Machine Casing", 8, 14, false)
-            .addOtherStructurePart("Glass", "Middle layer")
-            .addEnergyHatch("Any bottom casing", 1)
-            .addMaintenanceHatch("Any bottom casing", 1)
-            .addInputBus("Any bottom casing", 1)
-            .addOutputBus("Any bottom casing", 1)
+            .beginStructureBlock(7, 7, 13, false)
             .addSubChannelUsage(GTStructureChannels.BOROGLASS)
             .toolTipFinisher();
         return tt;
     }
 
     @Override
-    public ITexture[] getTexture(IGregTechTileEntity baseMetaTileEntity, ForgeDirection sideDirection,
-        ForgeDirection facingDirection, int colorIndex, boolean active, boolean redstoneLevel) {
-        if (sideDirection == facingDirection) {
-            if (active) return new ITexture[] { BlockIcons.casingTexturePages[0][16], TextureFactory.builder()
+    public ITexture[] getTexture(IGregTechTileEntity aBaseMetaTileEntity, ForgeDirection side, ForgeDirection aFacing,
+        int colorIndex, boolean aActive, boolean redstoneLevel) {
+        if (side == aFacing) {
+            if (aActive) return new ITexture[] { casingTexturePages[0][16], TextureFactory.builder()
                 .addIcon(OVERLAY_FRONT_ASSEMBLY_LINE_ACTIVE)
                 .extFacing()
                 .build(),
@@ -105,17 +139,35 @@ public class MTEAdvCircuitAssemblyLine extends MTEExtendedPowerMultiBlockBase<MT
                     .extFacing()
                     .glow()
                     .build() };
-            return new ITexture[] { BlockIcons.casingTexturePages[0][16], TextureFactory.builder()
+            return new ITexture[] { casingTexturePages[0][16], TextureFactory.builder()
                 .addIcon(OVERLAY_FRONT_ASSEMBLY_LINE)
                 .extFacing()
-                .build(),
-                TextureFactory.builder()
-                    .addIcon(OVERLAY_FRONT_ASSEMBLY_LINE_GLOW)
-                    .extFacing()
-                    .glow()
-                    .build() };
+                .build() };
         }
-        return new ITexture[] { BlockIcons.casingTexturePages[0][16] };
+
+        return new ITexture[] { casingTexturePages[0][16] };
+    }
+
+    public int getMultiplier() {
+        return multiplier;
+    }
+
+    public void setMultiplier(int multiplier) {
+        this.multiplier = multiplier;
+    }
+
+    @Override
+    public int getMaxParallelRecipes() {
+        return multiplier;
+    }
+
+    @Override
+    protected void setProcessingLogicPower(ProcessingLogic logic) {
+        // The voltage is only used for recipe finding
+        logic.setAvailableVoltage(Long.MAX_VALUE);
+        logic.setAvailableAmperage(1);
+        logic.setAmperageOC(true);
+        logic.setUnlimitedTierSkips();
     }
 
     @Override
@@ -131,25 +183,89 @@ public class MTEAdvCircuitAssemblyLine extends MTEExtendedPowerMultiBlockBase<MT
     @Override
     public boolean checkMachine(IGregTechTileEntity aBaseMetaTileEntity, ItemStack aStack) {
 
-        if (!checkPiece(STRUCTURE_PIECE_MAIN, 1, 2, 0)) return false;
+        if (!checkPiece(STRUCTURE_PIECE_MAIN, HORIZONTAL_OFFSET, VERTICAL_OFFSET, DEPTH_OFFSET)) return false;
 
         return mMaintenanceHatches.size() == 1;
     }
 
     @Override
     protected ProcessingLogic createProcessingLogic() {
-        return new ProcessingLogic().noRecipeCaching();
+        return new ProcessingLogic() {
+
+            BigInteger recipeEU;
+
+            @NotNull
+            @Override
+            protected CheckRecipeResult validateRecipe(@Nonnull GTRecipe recipe) {
+                BigInteger availableEU = getUserEU(ownerUUID);
+                long multiplier = (long) recipe.getMetadataOrDefault(GTRecipeConstants.EU_MULTIPLIER, 10);
+                recipeEU = BigInteger.valueOf(multiplier * recipe.mEUt * recipe.mDuration);
+                if (availableEU.compareTo(recipeEU) < 0) {
+                    finalConsumption = BigInteger.ZERO;
+                    return CheckRecipeResultRegistry.insufficientStartupPower(recipeEU);
+                }
+                maxParallel = availableEU.divide(recipeEU)
+                    .min(BigInteger.valueOf(maxParallel))
+                    .intValue();
+                return CheckRecipeResultRegistry.SUCCESSFUL;
+            }
+
+            @NotNull
+            @Override
+            protected CheckRecipeResult onRecipeStart(@Nonnull GTRecipe recipe) {
+                finalConsumption = recipeEU.multiply(BigInteger.valueOf(-calculatedParallels));
+                // This will void the inputs if wireless energy has dropped
+                // below the required amount between validateRecipe and here.
+                if (!addEUToGlobalEnergyMap(ownerUUID, finalConsumption)) {
+                    return CheckRecipeResultRegistry.insufficientStartupPower(finalConsumption);
+                }
+                // Energy consumed all at once from wireless net.
+                overwriteCalculatedEut(0);
+                return CheckRecipeResultRegistry.SUCCESSFUL;
+            }
+
+            @Nonnull
+            @Override
+            protected OverclockCalculator createOverclockCalculator(@Nonnull GTRecipe recipe) {
+                return OverclockCalculator.ofNoOverclock(recipe);
+            }
+        }.setMaxParallelSupplier(this::getTrueParallel);
+    }
+
+    @Override
+    public void onPreTick(IGregTechTileEntity aBaseMetaTileEntity, long aTick) {
+
+        super.onPreTick(aBaseMetaTileEntity, aTick);
+
+        if (aBaseMetaTileEntity.isServerSide() && (aTick == 1)) {
+            // Adds player to the wireless network if they do not already exist on it.
+            ownerUUID = processInitialSettings(aBaseMetaTileEntity);
+        }
     }
 
     @Override
     public void construct(ItemStack stackSize, boolean hintsOnly) {
-        buildPiece(STRUCTURE_PIECE_MAIN, stackSize, hintsOnly, 1, 2, 0);
+        buildPiece(STRUCTURE_PIECE_MAIN, stackSize, hintsOnly, HORIZONTAL_OFFSET, VERTICAL_OFFSET, DEPTH_OFFSET);
     }
 
     @Override
     public int survivalConstruct(ItemStack stackSize, int elementBudget, ISurvivalBuildEnvironment env) {
         if (mMachine) return -1;
-        return survivalBuildPiece(STRUCTURE_PIECE_MAIN, stackSize, 1, 2, 0, elementBudget, env, false, true);
+        return survivalBuildPiece(
+            STRUCTURE_PIECE_MAIN,
+            stackSize,
+            HORIZONTAL_OFFSET,
+            VERTICAL_OFFSET,
+            DEPTH_OFFSET,
+            elementBudget,
+            env,
+            false,
+            true);
+    }
+
+    @Override
+    protected @NotNull MTEMultiBlockBaseGui<?> getGui() {
+        return new MTEAdvancedCircuitAssemblyLineGui(this);
     }
 
     @Override
@@ -165,11 +281,6 @@ public class MTEAdvCircuitAssemblyLine extends MTEExtendedPowerMultiBlockBase<MT
     @Override
     public boolean supportsSingleRecipeLocking() {
         return true;
-    }
-
-    @Override
-    public Set<VoidingMode> getAllowedVoidingModes() {
-        return VoidingMode.ITEM_ONLY_MODES;
     }
 
 }
