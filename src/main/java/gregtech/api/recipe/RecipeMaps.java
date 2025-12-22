94dd56bc10ceb9b925909ee18065ec8fe5b56be0
mamiemru <mrunder.s@gmail.com>
2025-12-22 18:44:14 +0100

    chore: push code

--- src/main/java/gregtech/api/recipe/RecipeMaps.java
+++ src/main/java/gregtech/api/recipe/RecipeMaps.java
@@ -1320,6 +1320,25 @@ public final class RecipeMaps {
         .progressBar(GTUITextures.PROGRESSBAR_ARROW)
         .build();
 
+    public static final RecipeMap<RecipeMapBackend> cauldronRecipe = RecipeMapBuilder
+        .of("gt.recipe.cauldron", RecipeMapBackend::new)
+        .maxIO(1, 1, 1, 0)
+        .minInputs(1, 1)
+        .progressBar(GTUITextures.PROGRESSBAR_ARROW)
+        .neiRecipeBackgroundOffset(7, 0)
+        .neiRecipeBackgroundSize(165, 60)
+        .logoPos(150, 38)
+        .frontend(CauldronFrontend::new)
+        .neiHandlerInfo(builder -> {
+            builder.setWidth(170);
+            builder.setHeight(60);
+            builder.setDisplayStack(new ItemStack(Items.cauldron));
+            builder.setShowFavoritesButton(false);
+            builder.setShowOverlayButton(false);
+            return builder;
+        })
+        .build();
+
     public static final RecipeMap<RecipeMapBackend> advCircuitAssemblylineRecipes = RecipeMapBuilder
         .of("gt.recipe.advcircuitassemblyline")
         .maxIO(9, 8, 4, 0)
