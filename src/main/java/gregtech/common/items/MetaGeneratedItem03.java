312deb6c341955c70616fc66a08f2097e5a5ed73
mamiemru <mrunder.s@gmail.com>
2026-01-11 16:46:04 +0100

    chore: fluid is not deleted when rpstm fails

--- src/main/java/gregtech/common/items/MetaGeneratedItem03.java
+++ src/main/java/gregtech/common/items/MetaGeneratedItem03.java
@@ -22,6 +22,7 @@ import static gregtech.client.GTTooltipHandler.Tier.UV;
 import static gregtech.client.GTTooltipHandler.Tier.UXV;
 import static gregtech.client.GTTooltipHandler.Tier.ZPM;
 import static gregtech.client.GTTooltipHandler.registerTieredTooltip;
+import static gregtech.common.items.IDMetaItem03.*;
 import static gregtech.common.items.IDMetaItem03.Activated_Carbon_Filter_Mesh;
 import static gregtech.common.items.IDMetaItem03.Alumina_Support_Ring;
 import static gregtech.common.items.IDMetaItem03.Alumina_Support_Ring_Raw;
