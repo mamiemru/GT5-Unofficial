99888b804f7842180b31cd3866943314dc882690
mamiemru <mrunder.s@gmail.com>
2026-01-12 16:04:10 +0100

    chore: rename branch and push for backup

---
+++ src/main/java/gregtech/common/tileentities/machines/multi/acal/MTEACALupgradeBase.java
@@ -0,0 +1,37 @@
+package gregtech.common.tileentities.machines.multi.acal;
+
+import com.gtnewhorizon.structurelib.util.Vec3Impl;
+import gregtech.api.metatileentity.implementations.MTEEnhancedMultiBlockBase;
+import net.minecraft.util.StatCollector;
+
+import java.util.ArrayList;
+
+public abstract class MTEACALupgradeBase<T extends MTEEnhancedMultiBlockBase<T>> extends MTEModuleBase<T> {
+
+    protected MTEACALupgradeBase(int aID, String aName, String aNameRegional) {
+        super(aID, aName, aNameRegional);
+    }
+
+    public MTEACALupgradeBase(String aName) {
+        super(aName);
+    }
+
+    @Override
+    public String[] getInfoData() {
+        var ret = new ArrayList<String>();
+        // if this unit has a controller, give the coordinates
+        if (!controllerCoords.isEmpty()) {
+            for (Vec3Impl controllerCoord : controllerCoords) {
+                ret.add(
+                    StatCollector.translateToLocalFormatted(
+                        "GT5U.infodata.advanced_circuit_assembly_line_upgrade_base.linked_at",
+                        controllerCoord.get(0),
+                        controllerCoord.get(0),
+                        controllerCoord.get(0)));
+            }
+        } else ret.add(StatCollector.translateToLocal("GT5U.infodata.advanced_circuit_assembly_line_upgrade_base.not_linked"));
+        return ret.toArray(new String[0]);
+    }
+
+
+}
